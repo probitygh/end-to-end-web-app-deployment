d6293cdc6234557a6444d44602444806ef34e4b0
Awoke Emmanuel Idenyi <emmanuelawoke665@gmail.com>
2026-01-14 13:44:56 +0000

    Update configuration for Cognito and API

--- js/config.js
+++ js/config.js
@@ -1,12 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // eu-west-3_FbRnU1H2o
-        userPoolClientId: '', // 6pclio5goaetesqa0ng65be23q
-        region: '' // eu-west-3
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'eu-west-3_FbRnU1H2o',
+    userPoolClientId: '6pclio5goaetesqa0ng65be23q',
+    region: 'eu-west-3'
+  },
+  api: {
+    invokeUrl: 'https://YOUR_API_ID.execute-api.eu-west-3.amazonaws.com/prod'
+  }
 };
-
-
